18b5f5274232f344c944e27782138c849e3b5941
Neel Smith <dnsmith.neel@gmail.com>
2017-01-11 10:42:25 -0500

    Unit test all URN filtering on a Corpus

--- src/main/scala/edu/holycross/shot/ohco2/Corpus.scala
+++ src/main/scala/edu/holycross/shot/ohco2/Corpus.scala
@@ -4,10 +4,24 @@ import edu.holycross.shot.cite._
 import scala.io.Source
 
 case class Corpus (texts: Vector[CitableNode]) {
-  def urnMatch(filterUrn: CtsUrn) = {
+  def urnMatch(filterUrn: CtsUrn) : Vector[CitableNode]= {
     filterUrn.isRange match {
-      case true => throw Ohco2Exception("Range matching not yet implemented")
-      case false =>   texts.filter(_.urn.urnMatch(filterUrn))
+      // range filter:
+      case true => {
+        val u1 = CtsUrn(filterUrn.dropPassage.toString + filterUrn.rangeBeginRef)
+        val u2 = CtsUrn(filterUrn.dropPassage.toString + filterUrn.rangeEndRef)
+        try {
+          val idx1 = texts.indexOf(getFirstNode(u1))
+          val idx2 = texts.indexOf(getLastNode(u2)) + 1
+          texts.slice(idx1,idx2)
+        } catch {
+          case oe: Ohco2Exception => Vector.empty[CitableNode]
+        }
+      }
+      //node filter:
+      case false =>  {
+       texts.filter(_.urn.urnMatch(filterUrn))
+     }
     }
   }
   def getValidReff(filterUrn: CtsUrn): Vector[CtsUrn] = {
@@ -16,11 +30,21 @@ case class Corpus (texts: Vector[CitableNode]) {
   def getTextContents(filterUrn: CtsUrn, connector: String = "\n"): String = {
     urnMatch(filterUrn).map(_.text).mkString(connector)
   }
+  def getFirstNodeOption(filterUrn: CtsUrn): Option[CitableNode] = {
+    val matching = urnMatch(filterUrn)
+    matching.isEmpty match {
+      case true => None
+      case false => Some(matching.head)
+    }
+  }
   def getFirstNode(filterUrn: CtsUrn): CitableNode = {
-    urnMatch(filterUrn).head
+    getFirstNodeOption(filterUrn) match {
+      case None => throw Ohco2Exception("No node matching " + filterUrn)
+      case n: Some[CitableNode] => n.get
+    }
   }
   def getLastNode(filterUrn: CtsUrn): CitableNode = {
-    urnMatch(filterUrn).tail(1)
+    urnMatch(filterUrn).last
   }
 }
 
--- src/test/scala/edu/holycross/shot/ohco2/CorpusSpec.scala
+++ src/test/scala/edu/holycross/shot/ohco2/CorpusSpec.scala
@@ -6,41 +6,194 @@ import edu.holycross.shot.cite._
 
 class CorpusSpec extends FlatSpec {
 
-  "A corpus of citable texts"  should "have a non-empty vector of citable nodes" in pending
+  "A corpus of citable texts"  should "offer a constructor signature for instantiating a corpus from a 2-column delimited text file" in {
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    corpus match {
+      case c: Corpus => assert(true)
+      case _ => fail("Failed to create Corpus object")
+    }
+  }
+
+  it should "have a non-empty vector of citable nodes" in {
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    assert(corpus.texts.size > 0)
+  }
 
 
   // filtering on single node URNS:
-  it should "filter the corpus contents against a URN with matching work and passage hierarchies" in pending
+  it should "filter the corpus contents against a URN with matching work and passage hierarchies" in {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA.hmt:1.1.lemma")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    // result should be a single node with same URN:
+    assert (filtered.size == 1)
+    assert(filtered(0).urn == urn)
+  }
 
-  it should "filter the corpus contents against a  URN with matching work hierarchy and containing passage hierarchy" in pending
+  it should "filter the corpus contents against a  URN with matching work hierarchy and containing passage hierarchy" in {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA.hmt:1.1")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    // result should be two nodes with:
+    assert (filtered.size == 2)
+    // add test on trimmed version of URN...
+  }
 
-  it should "filter the corpus contents against a URN with containing work hierarchy and matching passage hierarchy" in pending
+  it should "filter the corpus contents against a URN with containing work hierarchy and matching passage hierarchy" in {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA:1.1.lemma")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    assert (filtered.size == 1)
+    // urns differ only in version:
+    assert (filtered(0).urn.textGroup == urn.textGroup)
+    assert (filtered(0).urn.work == urn.work)
+    assert (filtered(0).urn.passageComponent == urn.passageComponent)
+  }
 
 
-  it should "filter the corpus contents against a URN with containing work hierarchy and containing passage hierarchy" in pending
+  it should "filter the corpus contents against a URN with containing work hierarchy and containing passage hierarchy" in {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA:1.1")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    assert (filtered.size == 2)
+    for (txtnode <- filtered) {
+      assert (txtnode.urn.textGroup == urn.textGroup)
+      assert (txtnode.urn.work == urn.work)
+    }
+  }
 
+  it should "return an empty vector if the filtering URN does not appear in the corpus" in {
+      val urn = CtsUrn("urn:cts:fake:group:none")
+      val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+      val corpus = Corpus(srcFile,"\t")
+      val filtered = corpus.urnMatch(urn)
+      assert(filtered.isEmpty)
+  }
 
   // filtering on range URNS:
-  it should "filter the corpus contents against a range URN with matching work hierarchy and matching range end points" in pending
+  it should "filter the corpus contents against a range URN with matching work hierarchy and matching range end points" in  {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA.hmt:1.1.lemma-1.2.comment")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    assert (filtered.size == 4)
+    for (txtnode <- filtered) {
+      assert (txtnode.urn.textGroup == urn.textGroup)
+      assert (txtnode.urn.work == urn.work)
+      assert (txtnode.urn.version == urn.version)
+    }
+  }
 
-  it should "filter the corpus contents against a range URN with matching work hierarchy and containing range beginning point" in pending
+  it should "filter the corpus contents against a range URN with matching work hierarchy and containing range beginning point" in {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA.hmt:1.1-1.2.comment")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    assert (filtered.size == 4)
+    for (txtnode <- filtered) {
+      assert (txtnode.urn.textGroup == urn.textGroup)
+      assert (txtnode.urn.work == urn.work)
+      assert (txtnode.urn.version == urn.version)
+    }
+  }
 
-  it should "filter the corpus contents against a range URN with matching work hierarchy and containing range ending point" in pending
+  it should "filter the corpus contents against a range URN with matching work hierarchy and containing range ending point" in {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA.hmt:1.1.lemma-1.2")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    assert (filtered.size == 4)
+    for (txtnode <- filtered) {
+      assert (txtnode.urn.textGroup == urn.textGroup)
+      assert (txtnode.urn.work == urn.work)
+      assert (txtnode.urn.version == urn.version)
+    }
+  }
 
-  it should "filter the corpus contents against a range URN with matching work hierarchy and containing points for range beginning and ending" in pending
+  it should "filter the corpus contents against a range URN with matching work hierarchy and containing points for range beginning and ending" in {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA.hmt:1.1-1.2")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    assert (filtered.size == 4)
+    for (txtnode <- filtered) {
+      assert (txtnode.urn.textGroup == urn.textGroup)
+      assert (txtnode.urn.work == urn.work)
+      assert (txtnode.urn.version == urn.version)
+    }
+  }
 
-  it should "filter the corpus contents against a range URN with containing work hierarchy and matching range end points" in pending
+  it should "filter the corpus contents against a range URN with containing work hierarchy and matching range end points" in {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA:1.1.lemma-1.2.comment")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    assert (filtered.size == 4)
+    for (txtnode <- filtered) {
+      assert (txtnode.urn.textGroup == urn.textGroup)
+      assert (txtnode.urn.work == urn.work)
+    }
+  }
 
-  it should "filter the corpus contents against a range URN with containing work hierarchy and containing range beginning point" in pending
+  it should "filter the corpus contents against a range URN with containing work hierarchy and containing range beginning point" in {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA:1.1-1.2.comment")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    assert (filtered.size == 4)
+    for (txtnode <- filtered) {
+      assert (txtnode.urn.textGroup == urn.textGroup)
+      assert (txtnode.urn.work == urn.work)
+    }
+  }
 
-  it should "filter the corpus contents against a range URN with containing work hierarchy and containing range ending point" in pending
+  it should "filter the corpus contents against a range URN with containing work hierarchy and containing range ending point" in {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA:1.1.lemma-1.2")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    assert (filtered.size == 4)
+    for (txtnode <- filtered) {
+      assert (txtnode.urn.textGroup == urn.textGroup)
+      assert (txtnode.urn.work == urn.work)
+    }
+  }
 
-  it should "filter the corpus contents against a range URN with containing work hierarchy and containing points for range beginning and ending" in pending
+  it should "filter the corpus contents against a range URN with containing work hierarchy and containing points for range beginning and ending" in {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA:1.1-1.2")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    assert (filtered.size == 4)
+    for (txtnode <- filtered) {
+      assert (txtnode.urn.textGroup == urn.textGroup)
+      assert (txtnode.urn.work == urn.work)
+    }
+  }
 
+  it should "return an empty vector if the first node of the filtering URN does not appear in the corpus" in {
+    val urn = CtsUrn("urn:cts:greekLit:tlg5026.msA:FAKE-1.2")
+    val srcFile = "src/test/resources/scholia-twocolumns.tsv"
+    val corpus = Corpus(srcFile,"\t")
+    val filtered = corpus.urnMatch(urn)
+    assert (filtered.isEmpty)
+  }
+  it should "return an empty vector if the second node of the filtering URN does not appear in the corpus"
+
+  // CTS-like convenience methods
   it should "offer a convenience method for finding the first citable node in a filtered vector"
 
   it should "offer a convenience method for finding the last citable node in a filtered vector"
   it should "offer a convenience method for reducing a list of citable nodes to a list of URN"
   it should "offer a convenience method for extracting the string contents from a list of citable nodes as a single string"
-  it should "offer a constructor signature for instantiating a corpus from a 2-column delimited text file"
+
+
+
+
 }
