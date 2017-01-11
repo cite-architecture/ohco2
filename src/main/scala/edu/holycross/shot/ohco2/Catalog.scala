7459ee1c0f39a0e338ed147bd4dc6651d834460c
Neel Smith <dnsmith.neel@gmail.com>
2017-01-11 11:23:42 -0500

    Implement instantiating catalog from delimited text source

--- src/main/scala/edu/holycross/shot/ohco2/Catalog.scala
+++ src/main/scala/edu/holycross/shot/ohco2/Catalog.scala
@@ -1,7 +1,46 @@
 package edu.holycross.shot.ohco2
 
 import edu.holycross.shot.cite._
-
-case class Catalog (texts: Vector[CatalogEntry])
+import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Try
 
 case class CatalogEntry(urn: CtsUrn, citationScheme: String, groupName: String, workTitle: String, versionLabel: String, exemplarLabel: Option[String] = None, online: Boolean = true)
+
+
+case class Catalog (texts: Vector[CatalogEntry]) {
+  def forUrnOption(filterUrn: CtsUrn): Vector[CatalogEntry] = {
+    texts.filter(_.urn.urnMatch(filterUrn))
+  }
+}
+
+object Catalog {
+  def apply(f: String, sep: String = "#"): Catalog = {
+    var entries = scala.collection.mutable.ArrayBuffer.empty[CatalogEntry]
+    // read file, drop header line:
+    val columnsByRow = Source.fromFile(f).getLines.toVector.map(_.split(sep)).drop(1)
+
+    for (row <- columnsByRow) {
+      val urn = CtsUrn(row(0))
+      val citation = row(1)
+      val group = row(2)
+      val work = row(3)
+      val vers = row(4)
+      row.size match {
+        case 6 => {
+          val exemplar = None
+          val online = Try(row(5).toBoolean).getOrElse(false)
+          entries += CatalogEntry(urn,citation,group,work,vers,exemplar,online)
+        }
+        case 7 => {
+          val exemplar = Some(row(5))
+          val online = Try(row(6).toBoolean).getOrElse(false)
+          entries += CatalogEntry(urn,citation,group,work,vers,exemplar,online)
+        }
+      }
+    }
+    Catalog(entries.toVector)
+  }
+
+
+}
--- src/test/scala/edu/holycross/shot/ohco2/CatalogSpec.scala
+++ src/test/scala/edu/holycross/shot/ohco2/CatalogSpec.scala
@@ -7,13 +7,41 @@ import edu.holycross.shot.cite._
 */
 class CatalogSpec extends FlatSpec {
 
-  "A catalog of citable texts"  should "have a non-empty vector of catalog entries" in pending
+  "A catalog of citable texts" should "offer a constructor signature for instantiating a corpus for a delimited text file" in {
+    val srcFile = "src/test/resources/scholiacatalog.txt"
+    val catalog = Catalog(srcFile)
+    catalog match {
+      case c: Catalog => assert(true)
+      case _ => fail("Failed to create Catalog object")
+    }
+  }
+  it  should "have a non-empty vector of catalog entries" in {
+    val srcFile = "src/test/resources/scholiacatalog.txt"
+    val catalog = Catalog(srcFile)
+    assert(catalog.texts.size > 0)
+  }
+  it  should "have a convenience method to get entries by URN" in pending
 
-  "A catalog entry"  should "have a CtsUrn with a valid version identifier" in pending
+  "A catalog entry"  should "have a CtsUrn with a valid version identifier" in {
+    val srcFile = "src/test/resources/scholiacatalog.txt"
+    val catalog = Catalog(srcFile)
+    for (entry <- catalog.texts) {
+      entry.urn.versionOption match {
+        case None => fail("Failed to find matching version")
+        case _ => assert(true)
+      }
+    }
+  }
   it should "have a non-empty string for citation scheme " in pending
   it should "have a non-empty string for group name " in pending
   it should "have a non-empty string for work title " in pending
   it  should "have a non-empty string for version label " in pending
+
+  it should "throw an Ohco2 error if the URN does have a valid version identifier"
+  it should "throw an Ohco2 error if an empty citation scheme is given"
+  it should "throw an Ohco2 error if an empty group name is given"
+  it should "throw an Ohco2 error if an empty work title is given"
+  it should "throw an Ohco2 error if an empty version label is given"
 
   it should "have a none option for exemplarLabel when the urn is a version-level URN " in pending
   it should "have a non-empty string option for exemplarLabel when the urn is an exemplar-level URN " in pending
