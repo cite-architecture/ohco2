39cd5df2027ec7f0c46069167fb1a360989b7202
Neel Smith <dnsmith.neel@gmail.com>
2017-09-14 10:26:06 -0400

    Spec out similarity for all Chris' suggested test cases

--- shared/src/test/scala/edu/holycross/shot/ohco2/CorpusAlgebraSimilaritySpec.scala
+++ shared/src/test/scala/edu/holycross/shot/ohco2/CorpusAlgebraSimilaritySpec.scala
@@ -60,15 +60,59 @@ val textDelimited = "urn:cts:ns:tg.w.v1:1.1#Version_1:1.1\n" +
 
   "Similarity matching"  should  "find parents and cousins of an exemplar node" in {
     val urn = CtsUrn("urn:cts:ns:tg.w.v1.ex1:1.2.1")
-    println("Match against " + urn)
     val filtered = corpus ~~ urn
-    println(filtered.size)
-    println(filtered.nodes.map(_.urn).mkString("\n"))
-    val expectedSet = Set(CtsUrn("urn:cts:ns:tg.w.v1:1.2"), CtsUrn("urn:cts:ns:tg.w.v1.ex1:1.2.1"), CtsUrn("urn:cts:ns:tg.w.v1.ex2:1.2.1"))
+    val expectedSet = Set(
+      CtsUrn("urn:cts:ns:tg.w.v1:1.2"), CtsUrn("urn:cts:ns:tg.w.v1.ex1:1.2.1"), CtsUrn("urn:cts:ns:tg.w.v1.ex2:1.2.1"))
     assert (expectedSet == filtered.urns.toSet)
   }
 
+  it should "find parents and cousins of an exemplar container" in {
+    val urn = CtsUrn("urn:cts:ns:tg.w.v1:1.1")
+    println("For urn " + urn)
+    val filtered = (corpus ~~ urn.dropPassage) ~~ urn
+    // result should be seven nodes: the version ("1.1") and three from each exemplar (…:1.1.1, 1.1.2, 1.1.3)
+    assert (filtered.nodes.size == 7)
+    val expectedSet = Set(
+      CtsUrn("urn:cts:ns:tg.w.v1:1.1"),
+      CtsUrn("urn:cts:ns:tg.w.v1.ex1:1.1.1"),
+      CtsUrn("urn:cts:ns:tg.w.v1.ex1:1.1.2"),
+      CtsUrn("urn:cts:ns:tg.w.v1.ex1:1.1.3"),
+      CtsUrn("urn:cts:ns:tg.w.v1.ex2:1.1.1"),
+      CtsUrn("urn:cts:ns:tg.w.v1.ex2:1.1.2"),
+      CtsUrn("urn:cts:ns:tg.w.v1.ex2:1.1.3")
+    )
+    assert (expectedSet == filtered.urns.toSet)
+  }
+
+  it  should  "find parents and cousins of an exemplar-level range of containers" in {
+    val urn = CtsUrn("urn:cts:ns:tg.w.v1.ex1:1.1-1.2")
+    val filtered = (corpus ~~ urn)
+    assert(filtered.size == 30)
+  }
 
 
+  it should "find contained nodes and work-descendents of a version-level URN with a range" in {
+    val urn = CtsUrn("urn:cts:ns:tg.w.v1:1.2-1.3")
+    val filtered = corpus ~~ urn
+    assert(filtered.size == 32)
+  }
+
+
+  it should "find a version-level node when there are no exemplars" in {
+   val urn = CtsUrn("urn:cts:ns:tg.w.v2:1.2")
+   val filtered = corpus ~~ urn
+   // result should be one node
+   assert (filtered.nodes.size == 1)
+   assert(filtered.nodes(0).urn == urn)
+   }
+
+   it should "find version-level nodes in a range when there are no exemplars" in {
+     val urn = CtsUrn("urn:cts:ns:tg.w.v2:2.1-2.2")
+     val filtered = corpus ~~ urn
+     assert (filtered.nodes.size == 2)
+     println(filtered.urns.mkString("\n"))
+     val expected = Vector(CtsUrn("urn:cts:ns:tg.w.v2:2.1"), CtsUrn("urn:cts:ns:tg.w.v2:2.2"))
+     assert (filtered.urns == expected)
+   }
 
 }
